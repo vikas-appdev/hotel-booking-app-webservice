12769f86093ef298a5adc1dcbedc1d591e71ae25
vikasro <v.ray96610@gmail.com>
2018-09-11 14:32:12 +0530

    Lazy

--- src/main/java/com/sdigitizers/hotel/HotelApplication.java
+++ src/main/java/com/sdigitizers/hotel/HotelApplication.java
@@ -6,11 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import com.sdigitizers.hotel.fileupload.FIleStorageProperties;
 import com.sdigitizers.hotel.model.User;
 
 @SpringBootApplication
-@EnableConfigurationProperties({FIleStorageProperties.class})
+//@EnableConfigurationProperties({FIleStorageProperties.class})
 public class HotelApplication {
 
 	public static void main(String[] args) {
