84f1f289f7314f6cc382a7159f3065707dc716e3
vikasro <v.ray96610@gmail.com>
2018-09-18 18:05:08 +0530

    this is for user update

--- src/main/java/com/sdigitizers/hotel/controller/UserController.java
+++ src/main/java/com/sdigitizers/hotel/controller/UserController.java
@@ -13,11 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sdigitizers.hotel.UserNotFoundException;
+import com.sdigitizers.hotel.codec.BookingStatus;
+import com.sdigitizers.hotel.model.Booking;
+import com.sdigitizers.hotel.model.Transaction;
 import com.sdigitizers.hotel.model.User;
 import com.sdigitizers.hotel.repository.UserRepository;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
@@ -65,6 +69,25 @@ public class UserController {
 		
 	}
 	
+	@PutMapping("/users/{id}")
+	public ResponseEntity<Object> updateUser(@RequestBody User user,@PathVariable String id) {
+		
+		Optional<User> userOptional = userRepository.findByEmail(id);
+		if(!userOptional.isPresent())
+			throw new UserNotFoundException("id- "+id);
+
+		User user2 = userOptional.get();
+		
+		user.setId(user2.getId());
+		
+		
+		
+		
+		userRepository.save(user);
+
+		return ResponseEntity.noContent().build();
+		
+	}
 	
 
 }
