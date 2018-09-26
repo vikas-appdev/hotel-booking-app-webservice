2eba8abfbca69dbf9af8df5d479370e8248afe83
vikasro <v.ray96610@gmail.com>
2018-09-26 19:30:03 +0530

    Booking Checking

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ values(1,'Kuchh terms and conditions ','Khanapara', 'https://images.pexels.com/p
 insert into hotel(rating,terms,locality, image_link, id, city, country, latitude, longitude, pincode, state, street, contact_person, country_code, description, email, name, password, phone1, phone2)
 values(1,'Kuchh terms and conditions ','Khanapara', 'https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg', 10002, 'Jorhat', 'India', '19.2565', '20.2535', 785001, 'Assam', 'Fancy Ali', 'Malik Hotel Ka', '+91', 'Description wala column hai', 'vikas.appdev@gmail.com', 'Lakshmi Hotel', 'qwerty', 8723992495, 7002132366);
 
-insert into room(id, ac, active, category, clean_toilet, couple_allowed, discount_in_percentage, discount_value, family_allowed, name, no_of_persons, price, service24_7, tv, wifi, hotel_id)
-values(11001, 1, 1, 1, 1, 1, 1, 10, 1, 'test', 1, 500, 1, 1, 1, 10001);
+insert into room(id, ac, active, category,cctv,complementry_breakfast,couple_allowed,dining, discount_in_percentage, discount_value, family_allowed,geyser,heater,living_room,mini_fridge,name, no_of_persons,parking, price,resturant, service24_7, tv, wifi, hotel_id)
+values(11001, 1, 1, 1, 1, 1,1,1,1,100,1,1,1,1,1, 'test', 1,1, 500, 1, 1, 1,1,10001);
 insert into room(id, ac, active, category, clean_toilet, couple_allowed, discount_in_percentage, discount_value, family_allowed, name, no_of_persons, price, service24_7, tv, wifi, hotel_id)
 values(11002, 1, 1, 1, 1, 1, 0, 100, 1, 'test', 1, 500, 1, 1, 1, 10001);
 
